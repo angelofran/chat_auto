c3917257f9e5df34fbf24329c5f3f139722b48f1
Yuky オ <90131020+angelofran@users.noreply.github.com>
2023-02-25 10:14:32 +0100

    Updated

--- src/venv/Scripts/activate.bat
+++ src/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Desktop\chat_auto\venv
+set VIRTUAL_ENV=C:\Users\User\Desktop\chat_auto\src\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
