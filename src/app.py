4ac8cbd06605a06673b82db1b4289e437dad078a
Yuky オ <90131020+angelofran@users.noreply.github.com>
2023-02-25 10:35:13 +0100

    updated

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
 #Importação de biblioteca principal
 import openai as op
-
+import pyttsx3 as pt
+from time import sleep
+from read_text import read_text as rd
 
 #Configuração de chave
 op.api_key = "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"
+
 
 while True:
     print("-"*30)
@@ -20,13 +23,13 @@ while True:
         temperature=1, 
     )
 
-
     response = conclusão.choices[0].text
-    with open("text.txt", 'r', encoding='utf-8') as arq:
-        
-
-
-    print(f"Resposta: \t{response}.\n")
-    print("-"*30)
+    with open("src/text.txt", 'w', encoding='utf-8') as arq:
+        arq.write(response)
+        text = arq.read()
+        rd(text=text)
+        print(f"Resposta: \t{text}.\n")
+        print("-"*30)
+        sleep(10)
 
     
