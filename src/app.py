cf63d0f055996695359b0933b79b2c623df4faee
Yuky オ <90131020+angelofran@users.noreply.github.com>
2023-02-25 12:08:00 +0100

    Updated

--- src/app.py
+++ src/app.py
@@ -2,28 +2,7 @@
 import openai as op
 import pyttsx3 as pt
 from time import sleep
-import os
-
-def read_text(text):
-    engine = pt.init() # object creation
-
-    """ RATE"""
-    rate = engine.getProperty('rate')   # getting details of current speaking rate
-    engine.setProperty('rate', 125)     # setting up new voice rate
-
-
-    """VOLUME"""
-    volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)                   #printing current volume level
-    engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
-
-    """VOICE"""
-    voices = engine.getProperty('voices')       #getting details of current voice
-    #engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
-    engine.setProperty('voice', voices[0 ].id)   #changing index, changes voices. 1 for female
-
-    engine.say(text)
-    engine.runAndWait()
-    engine.stop()
+from say_text import read_text
 
 
 
@@ -49,5 +28,5 @@ while True:
     read_text(text=response)
     print(f"Response: \t{response}.\n")
     print("-"*30)
-    sleep(10)
+    sleep(3)
 
